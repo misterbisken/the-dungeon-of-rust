805e7ee2827c901d3c54691598c88ae356c83f5e
Misterbisken <zorro.andree@hotmail.com>
2026-04-29 02:40:56 +0200

    input fixat!

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("You awake in a dark dungeon!");
+    println!("You can go left, forward, right");
+    println!("What do you choose");
+
+    let mut choice = String::new();
+
+    io::stdin()
+        .read_line(&mut choice)
+        .expect("Pressed wrong button");
+
+    println!("You pressed to go forward");
 }
